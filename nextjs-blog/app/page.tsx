c8182f8393661c2614b96a57ecc37032347cbbd1
Alexander  Caughey <alca@tp-mac-c02fr78gmd6r.home>
2023-12-27 20:48:57 +0000

    fix: lint

--- nextjs-blog/app/page.tsx
+++ nextjs-blog/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
-          I'm <span className="font-bold">Alex</span>.
+          I&lsquo;m <span className="font-bold">Alex</span>.
         </span>
       </p>
     </main>
